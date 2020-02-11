eeca2cef370fc58c9ade0e5c6c1c8f77f5e13e21
Mirolim Mirzakhmedov <mirolim.mirzakhmedov@gmail.com>
2020-02-12 01:28:05 +0500

    add newseq done by reflection benchmark

--- testdata/newseq/main_test.go
+++ testdata/newseq/main_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/BurntSushi/ty/fun"
 	"github.com/mmirolim/gpp/macro"
 )
 
@@ -54,18 +55,35 @@ func TestNewSeqOpsHandWritten(t *testing.T) {
 	}
 }
 
+func TestNewSeqOpsByReflection(t *testing.T) {
+	fseq := []float64{100, 200, 300, 400, 500, 600}
+	type styp struct{ strLen int }
+	var out []styp
+
+	incout := fun.Map(func(v float64) float64 { return v + 1 }, fseq).([]float64)
+	filtered := fun.Filter(func(v float64) bool { return v < 300 }, incout).([]float64)
+	strs := fun.Map(func(v float64) string { return ftoa(v) }, filtered).([]string)
+	out = fun.Map(func(v string) styp { return styp{len(v)} }, strs).([]styp)
+
+	fmt.Printf("%+v\n", out) // output for debug
+
+	if len(out) != 2 {
+		t.Errorf("expected 3, got %d", len(out))
+	}
+}
+
 func BenchmarkNewSeqMacro(b *testing.B) {
 	fseq := []float64{100, 200, 300, 400, 500, 600}
 	type styp struct{ strLen int }
 	var out []styp
 	for i := 0; i < b.N; i++ {
+		out = out[:0]
 		macro.NewSeq_μ(fseq).Map(func(v float64) float64 { return v + 1 }).
 			Filter(func(v float64) bool { return v < 300 }).
 			Map(ftoa).
 			Map(func(v string, i int) styp { return styp{len(v) + i} }).
 			Ret(&out)
 	}
-	fmt.Printf("NewSeq %+v\n", len(out)) // output for debug
 }
 
 func BenchmarkNewSeqOpsHandWritten(b *testing.B) {
@@ -73,6 +91,7 @@ func BenchmarkNewSeqOpsHandWritten(b *testing.B) {
 	type styp struct{ strLen int }
 	var out []styp
 	for i := 0; i < b.N; i++ {
+		out = out[:0]
 		var incout []float64
 		// Map
 		for _, v := range fseq {
@@ -94,5 +113,17 @@ func BenchmarkNewSeqOpsHandWritten(b *testing.B) {
 			out = append(out, styp{len(v) + i})
 		}
 	}
-	fmt.Printf("NewSeqManual %+v\n", len(out)) // output for debug
+}
+
+func BenchmarkNewSeqOpsByReflection(b *testing.B) {
+	fseq := []float64{100, 200, 300, 400, 500, 600}
+	type styp struct{ strLen int }
+	var out []styp
+	for i := 0; i < b.N; i++ {
+		incout := fun.Map(func(v float64) float64 { return v + 1 }, fseq).([]float64)
+		filtered := fun.Filter(func(v float64) bool { return v < 300 }, incout).([]float64)
+		strs := fun.Map(func(v float64) string { return ftoa(v) }, filtered).([]string)
+		out = fun.Map(func(v string) styp { return styp{len(v)} }, strs).([]styp)
+	}
+	_ = out
 }
